06d83691d5c978c16eecae3b678bf199b5a42ade
Tsukasa Sato <tsukasasatodesu@gmail.com>
2024-10-12 22:18:14 +0300

    fixed wrong varviable name for GUILD_ID

--- src/register_commands.js
+++ src/register_commands.js
@@ -15,7 +15,7 @@ const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
 
     try {
         await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_IF),
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             {body: commands}
         )
 
