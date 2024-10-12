85154ac43a32f4822f85de2d8f275cd380681a82
Tsukasa Sato <tsukasasatodesu@gmail.com>
2024-10-13 00:06:47 +0300

    fixed isssue invalid string

--- src/register_commands.js
+++ src/register_commands.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
-const { REST, Routes } = require("discord.js");
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 
 const commands = [
-    {
-        name: 'Start Game',
-        description: "Start Connect Four",
-    },
-];
+    new SlashCommandBuilder()
+        .setName("game")
+        .setDescription("Start Connect Four"),
+].map(command => command.toJSON());
 
 const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
 
@@ -15,9 +14,12 @@ const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
 
     try {
         await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-            {body: commands}
-        )
+            Routes.applicationGuildCommands(
+              process.env.CLIENT_ID,
+              process.env.GUILD_ID
+            ),
+            { body: commands }
+          );
 
         console.log("slash commands registred");
     } catch(error) {
