b294a07d323058df0676eaf253064aff1d713dd7
Tsukasa Sato <tsukasasatodesu@gmail.com>
2024-10-12 20:09:23 +0300

    added command register

---
+++ src/register_commands.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const { REST, Routes } = require("discord.js");
+
+const commands = [
+    {
+        name: 'Start Game',
+        description: "Start Connect Four",
+    },
+];
+
+const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
+
+(async () => {
+    console.log("registring slash commands");
+
+    try {
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_IF),
+            {body: commands}
+        )
+
+        console.log("slash commands registred");
+    } catch(error) {
+        console.log(error);
+    }
+})();
